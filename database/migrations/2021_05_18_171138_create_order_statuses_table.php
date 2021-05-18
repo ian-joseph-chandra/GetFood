abecdc8b65167789ee16e96c4047e3cce9238c85
ianjoseph2204 <ianjoseph2204@gmail.com>
2021-05-19 03:01:55 +0800

    Update 2021_05_18_171138_create_order_statuses_table.php

--- database/migrations/2021_05_18_171138_create_order_statuses_table.php
+++ database/migrations/2021_05_18_171138_create_order_statuses_table.php
@@ -16,6 +16,7 @@ class CreateOrderStatusesTable extends Migration
         Schema::create('order_statuses', function (Blueprint $table) {
             $table->id();
             $table->string('name')->unique();
+            $table->text('description')->nullable();
             $table->timestamps();
         });
     }
