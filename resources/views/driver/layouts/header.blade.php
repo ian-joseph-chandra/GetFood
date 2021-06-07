19bf6ede6bfe55596fdc984e04ffd7b7bb00daf4
gga123 <61962097+gga1503@users.noreply.github.com>
2021-06-07 23:41:45 +0700

    update header.blade.php

--- resources/views/driver/layouts/header.blade.php
+++ resources/views/driver/layouts/header.blade.php
@@ -7,7 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>GetFood</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="{{ asset('css/restaurant/header.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/driver/header.css') }}">
 </head>
 
 <body>
